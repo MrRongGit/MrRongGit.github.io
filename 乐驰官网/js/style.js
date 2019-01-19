0c3da5f5a2681674810a3db5d55fd0e66ea724aa
Rong <3191494272@qq.com>
2019-01-19 16:09:13 +0800

    乐驰官网
    此网站乐驰

---
+++ 乐驰官网/js/style.js
@@ -0,0 +1,34 @@
+
+
+// $(function () {
+//     var w = $('.aa ul li').width()
+//     var n = $('.aa ul li').length
+//     var i = 0
+//     $('.aa ul').width(n * w)
+//     $('.move .left').click(function () {
+//         if (i > 0) {
+//             i--
+//         }
+        
+//         move()
+//     })
+//     $('.move .right').click(function () {
+//         if (i < n - 3) {
+//             i++
+//         }
+
+//         move()
+
+//     })
+
+    
+ 
+    
+//     function move() {
+//         $('.aa ul').animate({
+//             left: -i * w
+//         }, 500)
+        
+//     }
+    
+// })
